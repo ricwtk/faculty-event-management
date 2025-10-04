b33e16cf2763e7470b57fc4c7904f17fe2f2fe56
Richard Wong <ricwtk@gmail.com>
2025-10-04 23:25:27 +0800

    update pic edit component

--- src/components/PicEdit.vue
+++ src/components/PicEdit.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row items-center gap-2">
+  <div class="flex flex-row items-center gap-1">
     <div class="flex-none">
       <Button @click="confirmed = !confirmed" icon="pi" rounded link>
         <i class="pi pi-check text-green-500" v-if="confirmed"></i>
