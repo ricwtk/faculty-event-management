896c51383a6732cc8903710d49c74d9020bb151f
Richard Wong <ricwtk@gmail.com>
2025-10-21 11:09:04 +0800

    add permission dot

---
+++ src/components/PermissionDot.vue
@@ -0,0 +1,25 @@
+<template>
+  <i :class="['pi', `pi-${iconname}`]" :style="{ color: iconcolor }" :title="permission"></i>  
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps(["active", "permission"])
+
+const iconname = computed( () => {
+  switch (props.permission) {
+    case "view":
+      return "eye"
+    case "edit":
+      return "pencil"
+    case "create":
+      return "plus-circle"
+    case "delete":
+      return "minus-circle"
+    default:
+      return "circle-fill"
+  }
+})
+const iconcolor = computed( () => props.active ? 'green' : 'red' )
+</script>
